c7565ec349748dec4ad3c7847561e97e613d73ef
JoaoTurolla <programadorjoaoturolla@gmail.com>
2025-12-10 21:20:19 -0300

    Update: added References and worked on Chip8 Class

--- chip8.h
+++ chip8.h
@@ -2,7 +2,7 @@
 #include <random>
 #include <fstream>
 
-const unsigned int startAdress = 0x200;
+const unsigned int startAdress = 0x200; //Reason being the space needed for the interpreter, it should ocuppy from 0x000 to 0x1FF (511);
 
 class Chip8{
    public:
@@ -11,11 +11,17 @@ class Chip8{
       std::ifstream file(fileName, std::ios::binary, std::ios::ate);
 
       if(file.is_open()){
-         std::streampos size = file.tellg();
+         std::streampos size = file.tellg(); //since I opened the file from the end this should give me the size. Hopefully
          char* buffer = new char[size];
 
-         file.seekg(0, std::ios::beg);
-         //Got stuck here after 2 hours trying, not really used to file handling
+         file.seekg(0, std::ios::beg); //Got stuck here after 2 hours trying, not really used to file handling.
+         // Well, if I open a file surely I would read it... why didn't I do that earlier? Idk
+         file.read(buffer, size); //since size should compreheend the whole file, this should read all at once
+         file.close();
+
+
+         //delete[] buffer; Gotta let this here for a bit to shame on myself, great job! Read the file and delete what you've read
+         //imediatelly after
       }
    }
    void cycle();
